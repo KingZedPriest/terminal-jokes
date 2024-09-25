345680729ef91defc3cdb5fd151c92464cad50bd
Priest <Charleschukwuemeka47@gmail.com>
2024-09-25 15:35:22 +0100

    Feat: Add the getJoke function

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/rivo/tview"
 )
 
 var (
-	app *tview.Application
+	app      *tview.Application
+	textView *tview.TextView
 )
+
+func getAndDrawJoke() {
+	// Fetch joke from the web
+
+	//Update the UI with the joke
+	textView.Clear()
+	timeStr := fmt.Sprintf("[gray]%s", time.Now().Format(time.RFC1123))
+	fmt.Fprintln(textView, timeStr)
+}
 
 func main() {
 	app = tview.NewApplication()
