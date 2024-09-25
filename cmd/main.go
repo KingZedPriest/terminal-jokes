aee7d443229a37b2ec2803c52abdd8507605e1fb
Priest <Charleschukwuemeka47@gmail.com>
2024-09-25 18:25:59 +0100

    Feat: Add the fetch request

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 
 	"github.com/rivo/tview"
@@ -12,23 +15,56 @@ var (
 	textView *tview.TextView
 )
 
+type Payload struct {
+	Value string
+}
+
 func getAndDrawJoke() {
 	// Fetch joke from the web
+	result, err := http.Get("https://api.chucknorris.io/jokes/random?category=science")
+	if err != nil {
+		panic(err)
+	}
+
+	payloadBytes, err := io.ReadAll(result.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	payload := &Payload{}
+	err = json.Unmarshal(payloadBytes, payload)
+	if err != nil {
+		panic(err)
+	}
 
 	//Update the UI with the joke
 	textView.Clear()
-	timeStr := fmt.Sprintf("[gray]%s", time.Now().Format(time.RFC1123))
+	fmt.Fprintln(textView, payload.Value)
+	timeStr := fmt.Sprintf("\n\n[gray]%s", time.Now().Format(time.RFC1123))
 	fmt.Fprintln(textView, timeStr)
+}
+
+func refreshJoke() {
+	tick := time.NewTicker(time.Second * 5)
+	for {
+		select {
+		case <-tick.C:
+			getAndDrawJoke()
+			app.Draw()
+		}
+	}
 }
 
 func main() {
 	app = tview.NewApplication()
 
-	textView := tview.NewTextView().
+	textView = tview.NewTextView().
 		SetDynamicColors(true).
 		SetWrap(true).
 		SetTextAlign(tview.AlignCenter).
 		SetText("First Go App")
+
+	go refreshJoke()
 
 	if err := app.SetRoot(textView, true).Run(); err != nil {
 		panic(err)
